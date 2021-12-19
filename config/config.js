fc56985d7d9a932dfb46963d2bb88a2cce9e7beb
LennonOliveira29 <lennon.oliveira@useaurea.com>
2021-12-19 20:52:57 -0300

    adding all the data needed to communicate with the bank in the dot env file

--- config/config.js
+++ config/config.js
@@ -1,26 +1,35 @@
 require('dotenv').config();
-const { DB_HOST, DB_USER, DB_PASS } = process.env;
+const { 
+  DB_HOST, 
+  DB_USER, 
+  DB_PASS, 
+  DIALECT,
+  DB_DEV_NAME,
+  DB_TEST_NAME,
+  DB_PROD_NAME
+
+} = process.env;
 
 module.exports = {
   "development": {
     "username": DB_USER,
     "password": DB_PASS,
-    "database": "brave_ag_dev",
+    "database": DB_DEV_NAME,
     "host": DB_HOST,
-    "dialect": "postgres",
+    "dialect": DIALECT,
   },
   "test": {
     "username": DB_USER,
     "password": DB_PASS,
-    "database": "brave_ag_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    "database":  DB_TEST_NAME,
+    "host": DB_HOST,
+    "dialect": DIALECT
   },
   "production": {
     "username": DB_USER,
     "password": DB_PASS,
-    "database": "brave_ag_production",
+    "database": DB_PROD_NAME,
     "host": DB_HOST,
-    "dialect": "postgres"
+    "dialect": DIALECT
   }
 }
