6f6296d43cc91b8cd6bb3497256d8c327f3b7283
LennonOliveira29 <lennon.oliveira@useaurea.com>
2021-12-20 21:37:23 -0300

    Add tests in heros

--- tests/integration/controllers/hero.test.js
+++ tests/integration/controllers/hero.test.js
@@ -9,6 +9,10 @@ const { expect } = chai;
 const app = require('../../../index');
 const { Hero } = require('../../../models');
 
+const consoleLogStub = stub(console, 'log');
+before(()=> consoleLogStub.returns(true));
+after(()=> consoleLogStub.restore());
+
 describe('Retrieve all heroes', () => { 
     describe('When there is no registered Hero', () => {
         const findAllStub = stub(Hero, 'findAll');
@@ -44,3 +48,39 @@ describe('Retrieve all heroes', () => {
     })
     
 });
+
+describe('Route POST /hero', () => {
+  describe('when `body` data is valid', () => {
+    let hero;
+    let getHero;
+
+    before(async () => {
+      try {
+        hero = await chai.request(app)
+          .post('/hero')
+          .send({
+            content: "content test",
+          });
+
+        const { body : { id } } = hero;
+
+        getHero = await chai.request(app)
+          .get(`/hero/${id}`);
+      } catch (error) {
+        console.error(error.message);
+      }
+    });
+
+    it('should return 201 - Created', async () => {
+      const { status } = hero;
+      expect(status).to.be.equals(201);
+    });
+    it('returns an `id` attribute, which is a number', async () => {
+      const { body: { id } } = hero;
+
+      expect(typeof id).to.be.equals("number");
+    });
+  
+  });
+
+})
